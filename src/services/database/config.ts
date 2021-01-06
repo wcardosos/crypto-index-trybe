de373d04d8cb5b530ea8463805b41fe20813e5ac
Wagner Cardoso <wcardosos3421@gmail.com>
2021-01-05 22:53:57 -0300

    Adicionando comentário sobre o link do banco hardcoded

--- src/services/database/config.ts
+++ src/services/database/config.ts
@@ -1,6 +1,8 @@
 const config = {
     postgres: {
         client: 'pg',
+        // Em condições normais o link/credenciais do banco são pegas através de
+        // variáveis ambiente
         connection: 'postgres://ljwgzjvq:303VYDiU-RWZneP5jK4y1Ypad9YSurSY@motty.db.elephantsql.com:5432/ljwgzjvq'
     }
 }
