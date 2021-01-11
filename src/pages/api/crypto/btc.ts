f2b895d5edd4c9e018601cc7cde386e693527774
Wagner Cardoso <wcardosos3421@gmail.com>
2021-01-10 23:09:42 -0300

    Rota de atualizar valores de cotação

--- src/pages/api/crypto/btc.ts
+++ src/pages/api/crypto/btc.ts
@@ -1,6 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getBitcoinPriceController } from '../../../services/useCases/GetBitcoinPrice';
+import { updateCurrenciesValuesController } from '../../../services/useCases/UpdateCurrenciesValues';
 
 export default async function handle(request: NextApiRequest, response: NextApiResponse) {
-    return getBitcoinPriceController.handle(request, response);
+    if (request.method === 'GET') {
+        return getBitcoinPriceController.handle(request, response);
+    } else if (request.method === 'PUT') {
+        return updateCurrenciesValuesController.handle(request, response);
+    }
+
 }
